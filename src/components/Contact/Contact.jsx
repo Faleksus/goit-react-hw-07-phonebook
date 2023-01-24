a3c45ab944820abbef8ddb87c027bcc381f18793
Oleksiy Frolov <avralgroup@gmail.com>
2023-01-24 19:21:41 +0200

    update phonebook

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,6 @@
+
+export const Contact = ({ name, number}) => {
+    return (
+        <li>{name}: {number}</li>
+    )
+}
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { contactsSlice } from "./contacts/contacts.slice";
+import { filterSlice } from "./filter/filter.slice";
+
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsSlice.reducer,
+    filter: filterSlice.reducer
+  },
+});
+
+
